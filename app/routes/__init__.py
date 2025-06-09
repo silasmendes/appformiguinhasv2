e0ff8be33ba5f5167875badc4e5a323c9bdf9436
Silas Mendes <silasmendes@gmail.com>
2025-06-09 10:59:33 +0100

    Add condicoes moradia CRUD and tests

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,8 @@
 from app.routes.familia import bp as familia_bp
 from app.routes.contato import bp as contato_bp
+from app.routes.condicoes_moradia import bp as condicoes_moradia_bp
 
 def register_routes(app):
     app.register_blueprint(familia_bp)
     app.register_blueprint(contato_bp)
+    app.register_blueprint(condicoes_moradia_bp)
---
+++ app/schemas/condicoes_moradia.py
@@ -0,0 +1,21 @@
+from app import ma
+from app.models.condicoes_moradia import CondicaoMoradia
+
+
+class CondicaoMoradiaSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = CondicaoMoradia
+        load_instance = True
+
+    moradia_id = ma.auto_field(dump_only=True)
+    familia_id = ma.auto_field()
+    tipo_moradia = ma.auto_field()
+    valor_aluguel = ma.auto_field()
+    tem_agua_encanada = ma.auto_field()
+    tem_rede_esgoto = ma.auto_field()
+    tem_energia_eletrica = ma.auto_field()
+    tem_fogao = ma.auto_field()
+    tem_geladeira = ma.auto_field()
+    quantidade_camas = ma.auto_field()
+    quantidade_tvs = ma.auto_field()
+    quantidade_ventiladores = ma.auto_field()
