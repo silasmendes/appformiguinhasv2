2c3fa20c11d9c96fe384e55571b9000e9c3e6521
Silas Mendes <silasmendes@gmail.com>
2025-06-09 11:23:03 +0100

    Add saude_familiar CRUD and tests

---
+++ app/models/saude_familiar.py
@@ -0,0 +1,14 @@
+from app import db
+
+class SaudeFamiliar(db.Model):
+    __tablename__ = "saude_familiar"
+
+    saude_id = db.Column(db.Integer, primary_key=True)
+    familia_id = db.Column(db.Integer, nullable=False)
+    tem_doenca_cronica = db.Column(db.Boolean)
+    descricao_doenca_cronica = db.Column(db.Text)
+    usa_medicacao_continua = db.Column(db.Boolean)
+    descricao_medicacao = db.Column(db.Text)
+    tem_deficiencia = db.Column(db.Boolean)
+    descricao_deficiencia = db.Column(db.Text)
+    recebe_bpc = db.Column(db.Boolean)
