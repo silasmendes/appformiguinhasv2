d067ce98df5e873c2fe429b6403e049e120da926
Silas Mendes <silasmendes@gmail.com>
2025-06-09 15:44:59 +0100

    Adicionando arquivos de referencia para agilizar construcao de html

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+from flask import render_template
 from app import create_app
 
 app = create_app()
+
+@app.route("/")
+def home():
+    """Renderiza a página inicial."""
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
