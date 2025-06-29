299c4f01c46d9189a921ac4aea48e5a1cdca6c51
Silas Mendes <silasmendes@gmail.com>
2025-06-29 20:32:54 +0100

    Ajustando model Usuarios

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -10,7 +10,7 @@ class Usuario(db.Model, UserMixin):
     nome_completo = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128))
     tipo = db.Column(db.String(16), nullable=False)
-    senha_hash = db.Column(db.String(128), nullable=False)
+    senha_hash = db.Column(db.String(255), nullable=False)
     expires_at = db.Column(db.DateTime)
     last_login_at = db.Column(db.DateTime)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
