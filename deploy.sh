74152878fdba3b6182a2f18beb267c69a7a08600
Silas Mendes <silasmendes@gmail.com>
2025-07-09 14:14:24 +0100

    ajustando deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,16 @@ RESOURCE_GROUP="rgformiguinhasbr"
 ACR_NAME="crssm01docker"
 IMAGE_NAME="${APP_NAME}:latest"
 ACR_IMAGE="${ACR_NAME}.azurecr.io/${IMAGE_NAME}"
-WEBAPP_NAME="appformiguinhasweb"
+WEBAPP_NAME="formiguinhasbr"
 
 echo "🚧 Iniciando build da imagem..."
 docker build -t $APP_NAME .
 
 echo "🏷️  Tagueando imagem para o ACR..."
 docker tag $APP_NAME $ACR_IMAGE
+
+echo "🔗 Logando no Azure Container Registry..."
+az acr login --name $ACR_NAME
 
 echo "🚀 Enviando imagem para o ACR..."
 docker push $ACR_IMAGE
